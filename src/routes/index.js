01b6c9b885e2eca7691ef6d48d3bbb5c4b661b9f
Francisca <fraan928@gmail.com>
2022-02-10 12:51:33 -0300

    Using shopping cart routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import productRouter from './productRouter.js';
+import shopcartRouter from './shopcartRouter.js';
 
 const router = new Router();
 
 router.use(productRouter);
+router.use(shopcartRouter);
 
 export default router;
